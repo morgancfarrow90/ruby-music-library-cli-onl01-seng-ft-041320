d1dab1ede16443623a06cb7bec615665528dbe9a
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-09 21:13:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -59,8 +59,8 @@
 #   end
  end
  
- def self.new_from_filename(file_name)
-    song = file_name.split(" - ").tap do |song|
+ def self.new_from_filename(filename)
+    song = filename.split(" - ").tap do |song|
         song[2].chomp!(".mp3")
     artist = Artist.find_or_create_by_name(song[0])
     genre = Genre.find_or_create_by_name(song[2])
