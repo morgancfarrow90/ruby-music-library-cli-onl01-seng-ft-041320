12dcea50639f48f40167671196a6acf34fc85e64
Morgan Farrow <morgancfarrow@gmail.com>
2020-05-09 21:00:41 +0000

    Automatically backed up by Learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,7 +1,16 @@
 class MusicImporter
   
+  attr_accessor :path
+  
   def initialize(file_path)
-    @file_path = file_path
+    @path = file_path
   end
   
+  def files
+    @path
+  end 
+  
+  def self.import
+    
+  end
 end
